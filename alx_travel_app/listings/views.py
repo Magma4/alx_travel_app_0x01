13344e4a892ba298e8b4fb0c2b3143a4824a44aa
magma <youngmagma@protonmail.com>
2025-02-08 22:36:34 -0500

    Configured the urls for the routes

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Listing, Booking
+from .serializers import ListingSerializer, BookingSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+class ListingViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows listings to be viewed, created, updated, or deleted.
+    """
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class BookingViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows bookings to be viewed, created, updated, or deleted.
+    """
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
